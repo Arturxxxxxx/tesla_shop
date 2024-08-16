3605de657151b033c34695f7664045437a323c10
munarffxz <mmunarbek13@gmail.com>
2024-08-16 17:14:02 +0600

    settings

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['104.197.92.255']
+# ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'account.CustomUser'
 
 
