40cbf4dda6b37b6eabe6440a1affe2d4efd40cab
munarffxz <mmunarbek13@gmail.com>
2024-08-16 17:12:15 +0600

    gitignore

--- cards/models.py
+++ cards/models.py
@@ -1,12 +1,13 @@
-import datetime
 from django.db import models
+import datetime
 
 class Category(models.Model):
-    image = models.ImageField(upload_to='category/',blank=True, null=True)
+    image = models.ImageField(upload_to='category/', blank=True, null=True)
     category = models.CharField(max_length=50, unique=True, verbose_name="Категория")
 
     def __str__(self) -> str:
         return self.category
+
 
 class Product(models.Model):
     CHOICE_NEW = 'Новый'
@@ -16,16 +17,16 @@ class Product(models.Model):
         (CHOICE_NEW, 'Новый'),
         (CHOICE_USED, 'БУ'),
     ]
+
     image1 = models.ImageField(upload_to="cards/")
     image2 = models.ImageField(upload_to="cards/")
     image3 = models.ImageField(upload_to="cards/")
     image4 = models.ImageField(upload_to="cards/")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     price = models.PositiveIntegerField()
     description = models.TextField(max_length=300)
     artikul = models.PositiveIntegerField()
-    year = models.PositiveIntegerField() 
+    year = models.PositiveIntegerField()
     in_stock = models.BooleanField(default=True)
     model = models.CharField(max_length=50)
     spare_part_number = models.CharField(max_length=50)
@@ -35,7 +36,10 @@ class Product(models.Model):
         choices=CHOICE_OPTIONS,
         default=CHOICE_NEW,
     )
-    created_at = models.DateTimeField(default=datetime.datetime.now) 
+    created_at = models.DateTimeField(default=datetime.datetime.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  
 
     def __str__(self):
         return self.title
+
+
--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
             name='schema-swagger-ui'),
-    path('cards/', include('cards.urls')),
+    path('products/', include('cards.urls')),
     path('orders/',include('order.urls')),
     path('payment/',include('payment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
