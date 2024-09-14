564fb2336febacc5da2e93a3216439fa39cb7a30
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-09-14 20:01:20 +0600

    local

--- cards/models.py
+++ cards/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+import datetime
 
 class Category(models.Model):
     image = models.ImageField(upload_to='category/', blank=True, null=True)
@@ -8,21 +8,26 @@ class Category(models.Model):
     def __str__(self) -> str:
         return self.category
 
+
 class Product(models.Model):
     CHOICE_NEW = 'Новый'
     CHOICE_USED = 'Б/У'
     
     CHOICE_OPTIONS = [
         (CHOICE_NEW, 'Новый'),
-        (CHOICE_USED, 'Б/У'),
+        (CHOICE_USED, 'БУ'),
     ]
-    title = models.CharField(max_length=250)
+
+    image1 = models.ImageField(upload_to="cards/")
+    image2 = models.ImageField(upload_to="cards/")
+    image3 = models.ImageField(upload_to="cards/")
+    image4 = models.ImageField(upload_to="cards/")
+    title = models.CharField(max_length=255)
     price = models.PositiveIntegerField()
     description = models.TextField(max_length=300)
     artikul = models.PositiveIntegerField()
     year = models.PositiveIntegerField()
     in_stock = models.BooleanField(default=True)
-    marka = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     spare_part_number = models.CharField(max_length=50)
     generation = models.CharField(max_length=50)
@@ -31,13 +36,11 @@ class Product(models.Model):
         choices=CHOICE_OPTIONS,
         default=CHOICE_NEW,
     )
-    created_at = models.DateTimeField(default=timezone.now)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    images = models.JSONField(default=list, blank=True, null=True)
-   # images = models.ImageField(upload_to='posts_img', blank=True, verbose_name='Фото')
-
+    created_at = models.DateTimeField(default=datetime.datetime.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  
 
     def __str__(self):
         return self.title
+
 
 
--- cards/views.py
+++ cards/views.py
@@ -1,24 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Category,Product
 from .serializers import CategorySerializer,ProductSerializer
-from rest_framework import status
-from rest_framework.response import Response
 # Create your views here.
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer()
+    serializer_class = CategorySerializer
     
     
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer()
-    def create(self, request, *args, **kwargs):
-        try:
-            return super().create(request, *args, **kwargs)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
+    serializer_class = ProductSerializer
