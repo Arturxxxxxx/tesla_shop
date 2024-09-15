e84566913cde55339993c6b6a0a9418a8d8e0d81
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-09-15 16:20:03 +0600

    local

--- cards/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product
-from django.utils.safestring import mark_safe
-
-# Register your models here.
-
-admin.site.register(Category)
-admin.site.register(Product)
-
