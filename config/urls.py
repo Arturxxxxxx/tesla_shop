7e5d4131166c3d4a66eb4b025bcffbca1f625c85
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-10-03 02:06:59 +0600

    api

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,10 @@ from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register()
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,11 +24,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('basket/', include('basket.urls')),
-    path('products/', include('cards.urls')),
-    path('payment/',include('payment.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/account/', include('account.urls')),
+    re_path(r'^api/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/basket/', include('basket.urls')),
+    path('api/products/', include('cards.urls')),
+    path('api/payment/',include('payment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
