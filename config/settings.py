c73c1cfafd9c9d8cd41679a2d14acc765f545d23
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-10-03 16:38:20 +0600

    cors

--- config/settings.py
+++ config/settings.py
@@ -200,7 +200,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:9000",
     "http://localhost:3000",
-    "http://35.239.216.44",
+    "http://koreacenter.kg"
 ]
 
 CORS_ALLOW_METHODS = (
