6cd3d2ebb6cd5e8886eaa908f0d450d0a213349b
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-10-06 19:13:36 +0600

    basket

--- basket/urls.py
+++ basket/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BasketView,DeleteBasketItemView
 
 urlpatterns = [
-    path('carts', BasketView.as_view(), name='basket'),  # URL to view the basket
+    path('carts/', BasketView.as_view(), name='basket'),  # URL to view the basket
     path('item/<int:product_id>/', DeleteBasketItemView.as_view(), name='delete_basket_item'),
 
 ]
