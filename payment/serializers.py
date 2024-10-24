6991e2feb289546b4df570fa0721f98079de2d1b
Arturxxxxxx <dasxas3231@gmail.com>
2024-10-24 11:21:25 +0000

    gitignore

--- payment/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class PaymentSerializer(serializers.Serializer):
-    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
-    currency = serializers.CharField(default='usd')
-    source = serializers.CharField()  # Stripe token
--- payment/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PaymentView
-
-urlpatterns = [
-    path('payment/', PaymentView.as_view(), name='payment'),
-]
