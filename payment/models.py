eb512bd4a0d45c82029eda4f007dedb3ff7e2b3c
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-10-24 17:31:10 +0600

    payment

--- payment/models.py
+++ payment/models.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.db import models
 
 class Payment(models.Model):
-    firls_name = models.CharField(max_length=10)
-    last_name = models.CharField(max_length=10)
+    firls_name = models.CharField(max_length=10, null=True, blank=True)
+    last_name = models.CharField(max_length=10, null=True, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     stripe_charge_id = models.CharField(max_length=255)
