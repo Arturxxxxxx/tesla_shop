8dc0b03969bf493062424de4f40416ad80a8be7c
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-11-11 11:26:33 +0600

    mbank

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import StartPaymentSessionView, PaymentStatusView, FindSessionView
+from .views import StartPaymentSessionView, PaymentStatusView, FindSessionView, StartPaymentView, ConfirmPaymentView
 
 urlpatterns = [
     path("start-payment/", StartPaymentSessionView.as_view(), name="start_payment"),
     path("payment-status/<str:session_id>/", PaymentStatusView.as_view(), name="payment_status"),
-    path('find-session/<str:order_id>/', FindSessionView.as_view(), name='find_session')
+    path('find-session/<str:order_id>/', FindSessionView.as_view(), name='find_session'),
+    path('mbank-payment/', StartPaymentView.as_view(), name='mbank-payment'),
+    path('confirm-confirm/', ConfirmPaymentView.as_view(), name='confirm-confirm')
+
 ]
 
