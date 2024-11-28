f2c8e56dc6638693074295f86c3161132e3fead0
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-11-28 18:25:04 +0600

    e

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import StartPaymentSessionView, PaymentStatusView, FindSessionView, LastOrderDetailView
+from .views import StartPaymentSessionView, PaymentStatusView, FindSessionView, LastOrderDetailView, OrderCreateView
 
 urlpatterns = [
     path("start-payments/", StartPaymentSessionView.as_view(), name="start_payment"),
     path("payments-status/<str:session_id>/", PaymentStatusView.as_view(), name="payment_status"),
     path('find-session/<str:order_id>/', FindSessionView.as_view(), name='find_session'),
-    path('payments-history', LastOrderDetailView.as_view(), name='payments-history')
+    path('payments-history', LastOrderDetailView.as_view(), name='payments-history'),
+    path('orders/create/', OrderCreateView.as_view(), name='create-order'),
 ]
 
