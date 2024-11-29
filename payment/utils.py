094361a2eb2addd4d0b34b89b820a7f7dbb6d110
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-11-29 14:35:48 +0600

    Charged

--- payment/utils.py
+++ payment/utils.py
@@ -34,7 +34,7 @@ def check_payment_status(session_id):
     # Логирование ответа
     logger.info(f"Ответ от Payler для проверки статуса: {response_data}")
 
-    if response_data.get("status") == "Completed":
+    if response_data.get("status") == "Charged":
         payment_session = PaymentSession.objects.get(session_id=session_id)
         payment_session.status = "completed"
         payment_session.save()
