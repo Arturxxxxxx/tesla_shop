1ca846c22a0bf99861e13d15496671b5a1d01e25
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-11-30 21:57:43 +0600

    e

--- payment/models.py
+++ payment/models.py
@@ -43,7 +43,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    price_product = models.DecimalField(max_digits=10, decimal_places=3)
+    price_product = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)  # Цена товара на момент заказа
 
     def __str__(self):
