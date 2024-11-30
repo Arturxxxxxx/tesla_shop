f154f122ef1df4b26eefb12205d21ab921acdac1
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-11-30 13:52:39 +0600

    order

--- payment/serializers.py
+++ payment/serializers.py
@@ -1,34 +1,13 @@
 from rest_framework import serializers
-from .models import PaymentSession, Order, OrderItem
+from .models import PaymentSession, Order, OrderItem, Order
 from cards.models import Product 
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['title', 'price']  # Поля продукта
+        fields = ['name', 'price']  # Поля продукта
 
-class PaymentSessionSerializer(serializers.ModelSerializer):
-    products = ProductSerializer(many=True)
-    client_name = serializers.CharField(source='account.get_full_name', read_only=True)
-    client_phone = serializers.CharField(source='account.phone_number', read_only=True)
-    total_cost = serializers.SerializerMethodField()
-
-    class Meta:
-        model = PaymentSession
-        fields = [
-            'order_id', 'created_at', 'products', 'client_name',
-            'client_phone', 'status', 'total_cost'
-        ]
-
-    def get_total_cost(self, obj):
-        return sum(product.price for product in obj.products.all())
-
-
-
-    def get_client_name(self, ojb):
-        return f"{ojb.account.last_name} {ojb.account.first_name}"
-    
 
 class OrderItemSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name')
@@ -41,6 +20,7 @@ class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
     client_name = serializers.CharField(source='client.get_full_name')
     client_phone = serializers.CharField(source='client.phone_number')
+    client_address = serializers.CharField(source='client.address')
 
     class Meta:
         model = Order
@@ -60,27 +40,25 @@ class OrderItemCreateSerializer(serializers.ModelSerializer):
         model = OrderItem
         fields = ['product', 'quantity', 'price']  # Указываем только необходимые для создания поля
 
+class OrderCreateSerializer(serializers.ModelSerializer):
+    items = OrderItemCreateSerializer(many=True)
 
-# class OrderCreateSerializer(serializers.ModelSerializer):
-#     items = OrderItemCreateSerializer(many=True)
+    class Meta:
+        model = Order
+        fields = [
+            'client',
+            'total_amount',
+            'currency',
+            'client_phone',
+            'items'
+        ]
 
-#     class Meta:
-#         model = Order
-#         fields = [
-#             'client',
-#             'total_amount',
-#             'currency',
-#             'client_phone',
-#             'client_address',
-#             'items'
-#         ]
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')  # Убираем данные по товарам
+        order = Order.objects.create(**validated_data)  # Создаем заказ
 
-#     def create(self, validated_data):
-#         items_data = validated_data.pop('items')  # Убираем данные по товарам
-#         order = Order.objects.create(**validated_data)  # Создаем заказ
+        # Создаем элементы заказа (товары)
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
 
-#         # Создаем элементы заказа (товары)
-#         for item_data in items_data:
-#             OrderItem.objects.create(order=order, **item_data)
-
-#         return order
+        return order
