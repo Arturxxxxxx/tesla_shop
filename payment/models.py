3dd25c69cb294c4157c9f2d3b761523ea996ae36
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-12-01 02:29:56 +0600

    e

--- payment/models.py
+++ payment/models.py
@@ -25,15 +25,15 @@ class PaymentSession(models.Model):
 
 class Order(models.Model):
     STATUS_CHOICES = [
-        ('shipped', 'Отправлен'),
-        ('delivered', 'Доставлен')
+        ('Отправлен'),
+        ('Доставлен')
     ]
     order_id = models.CharField(max_length=50, unique=True)
     client = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
     order_date = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3, default='сом')
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Отправлен')
     client_phone = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
