6a2cef00b52467c75f41d20b0e63327e0b3c6bfb
Arturxxxxxx <154911923+Arturxxxxxx@users.noreply.github.com>
2024-12-25 13:44:11 +0600

    marka

--- cards/urls.py
+++ cards/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet, MarkaSerializer
+from .views import CategoryViewSet, ProductViewSet, MarkaViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'product', ProductViewSet)
-router.register(r'Marka', MarkaSerializer)  # Используем 'products' вместо 'product'
+router.register(r'Marka', MarkaViewSet)  # Используем 'products' вместо 'product'
 
 urlpatterns = [
     path('', include(router.urls))
